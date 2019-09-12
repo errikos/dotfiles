b0ed67cf26f9c3a80e7dae6eb2c9dfa22b835678
Ergys Dona <errikosd@gmail.com>
2019-09-12 22:45:21 +0200

    bash: add ls_colors for linux

--- bash/bash_config/00-prompt.bash
+++ bash/bash_config/00-prompt.bash
@@ -5,5 +5,6 @@ PROMPT_PATH="\w"
 
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
+export LS_COLORS="di=1;34:ln=1;35:so=1;31:pi=1;33:ex=1;32:bd=34;46:cd=34"
 
 export PS1="${PROMPT_USER}:${PROMPT_PATH}\n\\$ "
