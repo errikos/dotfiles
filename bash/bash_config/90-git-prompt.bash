b1a53edc0f8ec41072051d0facbcb5f52c616d02
Ergys Dona <5298184+errikos@users.noreply.github.com>
2022-08-04 10:19:38 +0200

    Update bash config

--- bash/bash_config/90-git-prompt.bash
+++ bash/bash_config/90-git-prompt.bash
@@ -3,5 +3,6 @@
 if [ -f "/opt/homebrew/opt/bash-git-prompt/share/gitprompt.sh" ]; then
   __GIT_PROMPT_DIR="/opt/homebrew/opt/bash-git-prompt/share"
   GIT_PROMPT_ONLY_IN_REPO=1
+  GIT_PROMPT_THEME=Solarized
   source "/opt/homebrew/opt/bash-git-prompt/share/gitprompt.sh"
 fi
