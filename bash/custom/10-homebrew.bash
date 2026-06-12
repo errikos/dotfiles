21bf0a68c80d4244f5a9c9885ed8f512bbd6d3e8
Ergys Dona <5298184+errikos@users.noreply.github.com>
2026-06-12 23:31:11 +0200

    homebrew: don't auto upgrade casks

--- bash/custom/10-homebrew.bash
+++ bash/custom/10-homebrew.bash
@@ -2,6 +2,7 @@
 #
 eval "$(/opt/homebrew/bin/brew shellenv bash)"
 export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_UPGRADE_AUTO_UPDATES_CASKS=1
 
 # enable bash completion
 [[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh"
